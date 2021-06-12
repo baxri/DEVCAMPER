04ffe8260571dfbbbad21f3c54362996cf2fc256
George <giorgi.bibilashvili89@gmail.com>
2021-06-12 20:45:10 +0400

    Dev camper

---
+++ notes.js
@@ -0,0 +1,13 @@
+const notes = {
+    "bcryptjs": "^2.4.3", // For hashing/cryping password field
+    "cookie-parser": "^1.4.4", // For working with cookies
+    "cors": "^2.8.5", // Enable and work with cors for express application
+    "dotenv": "^8.1.0", // .env variables configuration
+    "helmet": "^3.21.1", // Bunch of protection middlewares
+    "hpp": "^0.2.2", // HTTP Parameter Pollution attacks
+    "jsonwebtoken": "^8.5.1", // Json web tockes for authorisation and tokens generation
+    "mongoose": "^5.7.1", // ORM for mongoDB
+    "morgan": "^1.9.1",  // For logging requests
+    "slugify": "^1.3.5", // For slug and alias generations
+    "xss-clean": "^0.1.1" // Prevent XSS attacks
+}
